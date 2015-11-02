0cb804950b8ef507cb58478c3f7b0ed81e6d4d4f
takahashim <maki@rubycolor.org>
2015-11-03 02:03:18 +0900

    translate c -> ruby

--- main.rb
+++ main.rb
@@ -1,87 +1,78 @@
 #	PUT32(IRQ_DISABLE_BASIC,1);	//TODO いるかも？
 
-#	uart_init();
-	serial = Serial.new
+serial = Serial.new
 
-#	pwm_init();
-	# in1,in2,enable,pwm0or1
-	motorL = Motor.new(5,6,12,0)
-	motorR = Motor.new(19,16,20,1)
+# in1, in2, enable, pwm0or1
+motor_left = Motor.new(5,6,12,0)
+motor_right = Motor.new(19,16,20,1)
 
-#	timer_init();
-	time = SystemTimer.new
+SystemTimer.init
 
-#    spi_init();
-    gyro = Gyro.new
+gyro = Gyro.new() ## Gyro.new(Port::XX) ?
 
-#    "time, power,omegaI,thetaI,theta,omega,distance,vE5" という文字列をserialでかく
-	serial.write("time, power,omegaI,thetaI,theta,omega,distance,vE5")
+serial.puts("time,power,omegaI,thetaI,theta,omega,distance,vE5")
 
-#    while(1){
-#    	// gyroの値からモーターを制御する
-#    	MESURE_COUNTS = 45; //定数
+## gyroの値からモーターを制御する
+MESURE_COUNTS = 45 ## 定数
 
-#        mesureSum = 0;
-#        for(i = 0; i < MESURE_COUNTS; i++){
-#        	gyro_val = gyro.read(Y)
-#        	mesureSum = mesureSum + gyro_val;
-#        }
+rec_omega_i = [0]*10
 
-#        omegaI = mesureSum * 0.00875 / MESURE_COUNTS;
+loop do
+  mesure_sum = 0
+  MESURE_COUNTS.times do
+    mesure_sum += gyro.read(Gyro::Y)
+  end
 
-#        if(abs(omegaI) < 2){
-#        	omegaI = 0;
-#        }
-#        recOmegaI[0] = omegaI;
-#        thetaI = thetaI + omegaI;
-#        countS = 0;
-#        for(i = 0; i < 10; i++){
-#        	if(abs(recOmegaI[i]) < 4){
-#        		countS++;
-#        	}
-#        }
-#        if(countS > 9){
-#        	thetaI = 0;
-#        	vE5 = 0;
-#        	xE5 = 0;
-#        	sumPower = 0;
-#        	sumSumP = 0;
-#        }
-#        for(i = 9; i > 0; i-- ){
-#        	recOmegaI[i] = recOmegaI[i - 1];
-#        }
+  omega_i = mesure_sum * 0.00875 / MESURE_COUNTS;
 
-#        long t = kAngle * thetaI / 100;
-#        long o = kOmega * omegaI / 100;
-#        long s = kSpeed * vE5 / 1000;
-#        long d = kDistance * xE5 / 1000;
-#        powerScale = t + o + s + d;
-#//        power = max(min(95 * powerScale / 100, 255), -255);
-#        long ltmp = 95 * powerScale / 100;
-#        if(ltmp > 255){
-#        	ltmp = 255;
-#        }
-#        if(ltmp < -255){
-#        	ltmp = -255;
-#        }
-#        power = ltmp;
-#        sumPower = sumPower + power;
-#        sumSumP = sumSumP + sumPower;
-#        vE5 = sumPower;
-#        xE5 = sumSumP / 1000;
+  if omega_i.abs < 2
+    omega_i = 0
+  end
+  rec_omega_i[0] = omega_i
+  theta_i += omega_i
 
-        #motorに渡す
-#        if(power > 0){
-#        	#正転
-#        	motorL.drive(FOWARD, power)
-##        	motorR.drive(FOWARD, power)
-#        }else{
-#        	#逆転
-#        	motorL.drive(REVERSE, -1 * power)
-#        	motorR.drive(REVERSE, -1 * power)
-#        }
+  count_s = 0
+  10.times do |i|
+    if rec_omega_i[i].abs < 4
+      count_s+=1
+    end
+  end
 
-#//        unsigned long now = (unsigned long)((get_systime() -init_t) / 1000);
-#        sprintf(buf, "%ld,%ld,%d,%ld,%ld,%ld%ld,%ld,%ld", now, power, omegaI, (thetaI/286), t, o, (d/200),vE5);
-#        serial.write(buf)
-#    }
+  if count_s > 9
+    theta_i = 0
+    v_e5 = 0
+    x_e5 = 0
+    sum_power = 0;
+    sum_sum_power = 0;
+  end
+  9.stepto(1, -1) do
+    rec_omega_i[i] = rec_omega_i[i - 1]
+  end
+
+  t = k_angle * theta_i / 100.0
+  o = k_omega * omega_i / 100.0
+  s = k_speed * v_e5 / 1000.0
+  d = k_distance * xE5 / 1000.0
+  power_scale = t + o + s + d
+  ##  power = max(min(95 * powerScale / 100, 255), -255)
+  ltmp = 95 * power_scale / 100.0
+  if ltmp > 255
+    ltmp = 255
+  end
+  if ltmp < -255
+    ltmp = -255
+  end
+  power = ltmp
+  sum_power += power
+  sum_sum_power += sum_power
+  v_e5 = sum_power
+  x_e5 = sum_sum_power / 1000.0
+
+  # motorに渡す
+  motor_left.drive(power) ## powerがマイナスなら逆転
+  motor_right.drive(power)
+
+  now = SystemTimer.now
+  buf = sprintf("%ld,%ld,%d,%ld,%ld,%ld%ld,%ld,%ld", now, power, omegaI, (theta_i/286), t, o, (d/200),v_e5)
+  serial.puts(buf)
+end
