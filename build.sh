f598112b521eaf4861e64aef8e84d86b483054fb
takahashim <maki@rubycolor.org>
2015-11-05 19:50:43 +0900

    Array.new is simpler than [0]*10

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-/Users/yuri/git/mruby/bin/mrbc -g -Bcode -omain_rb.h main.rb
+#/Users/yuri/git/mruby/bin/mrbc -g -Bcode -omain_rb.h main.rb
+/Users/maki/rc/mruby_rs_robot/mruby/bin/mrbc -g -Bcode -omain_rb.h main.rb
