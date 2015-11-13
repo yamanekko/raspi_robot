b226a323b9adbbb821d6554e70f4e7f790f2bca0
yuri <yamayuri@gmail.com>
2015-11-13 19:05:19 +0900

    motor demo sample

--- motor-test.rb
+++ motor-test.rb
@@ -2,11 +2,25 @@
 motor_left = Motor.new(5,6,12,0)
 motor_right = Motor.new(16,20,19,1)
 
+motor_right.drive(10)
+motor_left.drive(10) ## powerがマイナスなら逆転
+RSRobot.delay(3000000)
+motor_left.drive(20)
+motor_right.drive(20)
+RSRobot.delay(3000000)
+motor_right.drive(50)
+motor_left.drive(50) ## powerがマイナスなら逆転
+RSRobot.delay(3000000)
+motor_right.drive(70)
+motor_left.drive(70) ## powerがマイナスなら逆転
+RSRobot.delay(3000000)
+motor_left.drive(100)
 motor_right.drive(100)
-motor_left.drive(100) ## powerがマイナスなら逆転
-RSRobot.delay(0x1000000)
-motor_left.drive(200)
-motor_right.drive(200)
-RSRobot.delay(0x1000000)
+RSRobot.delay(3000000)
+motor_right.drive(-50)
+motor_left.drive(-50) ## powerがマイナスなら逆転
+RSRobot.delay(3000000)
+motor_left.drive(-200)
+motor_right.drive(-200)
 motor_left.stop
 motor_right.stop
